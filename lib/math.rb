5a7f159753bb0d192fbb1b851d397dd24937a0b2
Saif Minhas <saif_minhas@hotmail.com>
2020-04-02 12:41:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ num1 - num2
 end
 
 def division(num1, num2)
-num1%num2
+num1/num2
 end
 
 def multiplication(num1, num2)
@@ -15,7 +15,7 @@ num1*num2
 end
 
 def modulo(num1, num2)
-
+num1%num2
 end
 
 def square_root(num)
